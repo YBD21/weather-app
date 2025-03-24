7d0f10b982a6cf313f26384837cf31d5b72790bb
YBD21 <santosh.deuja2000@gmail.com>
2025-03-24 10:31:49 +0545

    added react query

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,14 +2,19 @@ import { Stack } from "expo-router";
 import "react-native-reanimated";
 import "../../global.css";
 import { StatusBar } from "expo-status-bar";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useReactQueryDevTools } from "@dev-plugins/react-query";
+
+const queryClient = new QueryClient({});
 
 export default function RootLayout() {
+  useReactQueryDevTools(queryClient);
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Stack screenOptions={{ headerShown: true, headerTitleAlign: "center" }}>
         <Stack.Screen name="+not-found" options={{ headerShown: true }} />
       </Stack>
       <StatusBar style="auto" />
-    </>
+    </QueryClientProvider>
   );
 }
