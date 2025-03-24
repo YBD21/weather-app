703702cab0b6b437981d06a9c5f9bc3f7f13a53d
YBD21 <santosh.deuja2000@gmail.com>
2025-03-24 15:05:50 +0545

    added endpoints

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,16 +1,25 @@
 import { apiKey } from "../constants";
 import client from "./axiosClient";
 
-export const getForecast = async (params: any) => {
-  const response = await client().get(`/forecast?id=524901&appid=${apiKey}`, {
-    params,
-  });
+interface ForecastParams {
+  cityName: string;
+  days: number;
+}
+
+interface LocationParams {
+  cityName: string;
+}
+
+export const getForecast = async ({ cityName, days }: ForecastParams) => {
+  const response = await client().get(
+    `/forecast.json?key=${apiKey}&q=${cityName}&days=${days}&aqi=no&alerts=no`
+  );
   return response.data;
 };
 
-export const getlocation = async (params: any) => {
-  const response = await client().get(`/search?id=524901&appid=${apiKey}`, {
-    params,
-  });
+export const getlocation = async ({ cityName }: LocationParams) => {
+  const response = await client().get(
+    `/forecast.json?key=${apiKey}&q=${cityName}`
+  );
   return response.data;
 };
